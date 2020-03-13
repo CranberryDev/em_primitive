8f33275cbb268f0be9f071099ee4c0a3a32be3b6
Agaev Artem <Agaev.A.R@sberbank.ru>
2020-03-13 10:35:21 +0300

    added sql scripts

---
+++ src/main/java/com/example/schedulingtasks/domain/entity/Note.java
@@ -0,0 +1,14 @@
+package com.example.schedulingtasks.domain.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@Getter
+@Setter
+public class Note {
+}
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -12,3 +12,29 @@ create table if not exists test."user"
   is_applied boolean,
   date       date
 );
+
+create table if not exists test."note"
+(
+  id   bigserial     not null
+    constraint note_id_pk primary key,
+  text varchar(2048) not null
+);
+
+create table if not exists test."access_level"
+(
+  value varchar(128) primary key
+);
+
+create table if not exists test."user_note"
+(
+  id      bigserial not null
+    constraint user_note_pk primary key,
+  user_id bigint    not null,
+  note_id bigint    not null,
+  access_level varchar(128) not null,
+
+  constraint user_note_user_fk foreign key (user_id) REFERENCES test."user" (id),
+  constraint user_note_note_fk foreign key (note_id) references test."note" (id),
+  constraint user_note_access_level_fk foreign key (access_level) references test."access_level" (value)
+);
+
